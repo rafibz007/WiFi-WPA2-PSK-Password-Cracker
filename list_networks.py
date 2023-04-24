5d47691f07bdd57a38ea07229725410ba0162703
Rafał Kamiński <r.kaminski008@gmail.com>
2023-04-24 20:13:07 +0200

    Refactor
    Change main.py to list_networks.py in order to work on seperate
    functionalities in other files as separate scripts

--- list_networks.py
+++ list_networks.py
@@ -1,21 +1,12 @@
-import socket
-from itertools import cycle
 from threading import Thread
-from time import sleep
 
 from core.filters import *
 from core.frame import ManagementFrame, ManagementFrameBody
 from core.sniffer import PacketSniffer
-from utils.interface import *
-from core.parser import EthernetParser, RawDataParser, ManagementFrameParser
+from core.parser import ManagementFrameParser
+from utils.interface import alternate_channels
 
 iface = "wlp4s0mon"
-
-def alternate_channels():
-    for i in cycle([1, 3, 5, 9, 10, 11, 12, 13, 36, 40]):
-        change_channel(iface, i)
-        sleep(0.1)
-
 
 packet_sniffer = PacketSniffer(
     iface,
@@ -30,12 +21,7 @@ packet_sniffer = PacketSniffer(
     )
 )
 
-# ssid listing
-# in second thread change channels and send probe requests (another interface may be neccessary)
-# here receive filtered and parsed probe responses and beacon frames
-# RUNS IN MONITOR MODE
-# change_channel()
-thread = Thread(target=alternate_channels, args=(), daemon=True)
+thread = Thread(target=alternate_channels, args=(iface,), daemon=True)
 thread.start()
 
 found_aps = {}
@@ -43,16 +29,7 @@ found_aps = {}
 print(f"{'BSSID' : >20} | {'FREQUENCY' :^10} | {'SSID' : <30}")
 print(f"{'':->20}-+-{'':-^10}-+-{'':-<30}")
 for packet in packet_sniffer.listen():
-    # print(packet)
     packet: ManagementFrame
     if packet.body.info_elements[ManagementFrameBody.SSID][1] not in found_aps:
         found_aps[packet.body.info_elements[ManagementFrameBody.SSID][1]] = packet.bssid
         print(f"{packet.bssid : >20} | {packet.radio_tap_header.channel_frequency :^10} | {packet.body.info_elements[ManagementFrameBody.SSID][1] : <30}")
-
-    # packet_sniffer.send()
-
-# 4-way handshake grabing
-# listen for authed users then
-# in second thread send deauth requests
-# here receive all filtered 4-way handshake requests then start password cracking
-# todo
--- utils/interface.py
+++ utils/interface.py
@@ -1,20 +1,13 @@
 import os
-
-
-def turn_on_monitor_mode(iface: str):
-    # os.system(f"sudo ifconfig {iface} down")
-    # os.system(f"sudo iwconfig {iface} mode monitor")
-    # os.system(f"sudo ifconfig {iface} up")
-    os.system(f"sudo airmon-ng check kill")
-    os.system(f"sudo airmon-ng start {iface}")
-
-
-def turn_off_monitor_mode(iface: str):
-    # os.system(f"sudo ifconfig {iface} down")
-    # os.system(f"sudo iwconfig {iface} mode managed")
-    # os.system(f"sudo ifconfig {iface} up")
-    os.system(f"sudo airmon-ng stop {iface}")
+from itertools import cycle
+from time import sleep
 
 
 def change_channel(iface: str, channel: int):
     os.system(f"sudo iwconfig {iface} channel {channel}")
+
+
+def alternate_channels(iface: str):
+    for i in cycle([1, 3, 5, 9, 10, 11, 12, 13, 36, 40]):
+        change_channel(iface, i)
+        sleep(0.1)
